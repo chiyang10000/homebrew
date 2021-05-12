879ff8ebccaf5301c9798986bb77c929c0ef639c
Chiyang Wan <chiyang10000@gmail.com>
2021-05-12 11:03:42 +0800

    Change repo path

--- planchecker.rb
+++ planchecker.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Planchecker < Formula
-  url "http://yum.oushu-tech.com/oushurepo/yumrepo/internal/linux/toolchain/planchecker-1.0.0.tar.xz"
+  url "http://yumazure.oushu-tech.com:12000/oushurepo/yumrepo/internal/linux/toolchain/planchecker-1.0.0.tar.xz"
   sha256 "7fe66166fb7cdf95b0f35b67b977c4d14af6c5ba7ce02ca257870720a1ab8068"
 
   def install
