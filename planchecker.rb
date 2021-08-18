d69d89631efc00649b7b90417403ffaa8c7e810c
Chiyang Wan <chiyang10000@gmail.com>
2021-08-18 08:46:12 +0800

    change repo to github

--- planchecker.rb
+++ planchecker.rb
@@ -1,8 +1,8 @@
 require "formula"
 
 class Planchecker < Formula
-  url "http://yumazure.oushu-tech.com:12000/oushurepo/yumrepo/internal/linux/toolchain/planchecker-1.0.0.tar.xz"
-  sha256 "7fe66166fb7cdf95b0f35b67b977c4d14af6c5ba7ce02ca257870720a1ab8068"
+  url "https://github.com/chiyang10000/homebrew-tap/releases/download/planchecker-1.0.1/planchecker-1.0.1.tar.xz"
+  sha256 "7109fdf1635da25b2382a70a4de6c84cc06722fc2a6bd008ebfa33ad36a2dfbd"
 
   def install
     bin.install "planchecker"
