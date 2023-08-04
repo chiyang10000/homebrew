9a8d3187fc879e154d20aaa235be566b1c55549c
WAN <chiyang10000@gmail.com>
2023-08-04 17:15:59 +0800

    update oushudb tar with github release

--- oushudb.rb
+++ oushudb.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 class Oushudb < Formula
-  url "http://yumazure.oushu-tech.com:12000/oushurepo/yumrepo/internal/linux/toolchain/bak/OushuDB-Darwin-2020-12-08.tar.xz"
+  url "https://github.com/chiyang10000/homebrew-tap/releases/download/libhadoop/OushuDB-Darwin-2020-12-08.tar.xz"
   sha256 "04065ce5d0f9fb8627a230902b38322a14af9f72451ea7ab3502326e1dff630c"
   version "2020-12-08"
 
